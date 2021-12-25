5e612e284afdcdc5d126a3028507916397b2ffac
overbid <overbid@gmail.com>
2021-12-26 01:10:28 +0700

    reformat

--- pmpi.py
+++ pmpi.py
@@ -10,7 +10,8 @@ from sds011 import *
 
 def temperature_get():
     return_value = subprocess.check_output(
-        "sudo vcgencmd measure_temp", stderr=subprocess.STDOUT, shell=True)
+        "sudo vcgencmd measure_temp", stderr=subprocess.STDOUT, shell=True
+    )
     return_text = str(return_value)
     return_list = re.findall(r"(?<==).*?(?=\')", return_text)
 
@@ -27,8 +28,8 @@ def pm_query(n=3):
         pm_2_5 += results[0]
         pm_10 += results[1]
         time.sleep(6)
-    pm_2_5 = round(pm_2_5/n, 1)
-    pm_10 = round(pm_10/n, 1)
+    pm_2_5 = round(pm_2_5 / n, 1)
+    pm_10 = round(pm_10 / n, 1)
 
     return pm_2_5, pm_10
 
@@ -43,36 +44,50 @@ def aqi_convert(pm_2_5, pm_10):
 def mqtt_sent(pm_2_5, pm_10, aqi_2_5, aqi_10, temp):
 
     try:
-        channel_id = config('CHANNEL_ID')
-        apikey_write = config('APIKEY_WRITE')
-        mqtt_urn = 'channels/' + channel_id + '/publish/' + apikey_write
-        mqtt_hostname = config('MQTT_HOSTNAME')
-        mqtt_transport = config('MQTT_TRANSPORT')
-        mqtt_port = int(config('MQTT_PORT'))
-        mqtt_tsl = config('MQTT_TSL')
-        mqtt_tsl = None if mqtt_tsl == 'None' else mqtt_tsl
+        channel_id = config("CHANNEL_ID")
+        apikey_write = config("APIKEY_WRITE")
+        mqtt_urn = "channels/" + channel_id + "/publish/" + apikey_write
+        mqtt_hostname = config("MQTT_HOSTNAME")
+        mqtt_transport = config("MQTT_TRANSPORT")
+        mqtt_port = int(config("MQTT_PORT"))
+        mqtt_tsl = config("MQTT_TSL")
+        mqtt_tsl = None if mqtt_tsl == "None" else mqtt_tsl
     except:
-        print('[ERROR]', datetime.now(), "Can't get .env file.", sep=',')
+        print("[ERROR]", datetime.now(), "Can't get .env file.", sep=",")
 
-    mqtt_playload = 'field1=' + str(pm_2_5) + '&field2=' + str(
-        aqi_2_5) + '&field3=' + str(pm_10) + '&field4=' + str(aqi_10) + '&field5=' + temp
-    publish.single(mqtt_urn, payload=mqtt_playload, hostname=mqtt_hostname,
-                   port=mqtt_port, tls=mqtt_tsl, transport=mqtt_transport)
+    mqtt_playload = (
+        "field1="
+        + str(pm_2_5)
+        + "&field2="
+        + str(aqi_2_5)
+        + "&field3="
+        + str(pm_10)
+        + "&field4="
+        + str(aqi_10)
+        + "&field5="
+        + temp
+    )
+    publish.single(
+        mqtt_urn,
+        payload=mqtt_playload,
+        hostname=mqtt_hostname,
+        port=mqtt_port,
+        tls=mqtt_tsl,
+        transport=mqtt_transport,
+    )
 
 
-sensor = SDS011('/dev/ttyUSB0', use_query_mode=True)
+sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
 
 try:
     pm_2_5, pm_10 = pm_query(3)
     aqi_2_5, aqi_10 = aqi_convert(pm_2_5, pm_10)
     temp = temperature_get()
     mqtt_sent(pm_2_5, pm_10, aqi_2_5, aqi_10, temp)
-    print('[INFO]', datetime.now(), pm_2_5,
-          pm_10, aqi_2_5, aqi_10, temp, sep=',')
+    print("[INFO]", datetime.now(), pm_2_5, pm_10, aqi_2_5, aqi_10, temp, sep=",")
 except TypeError:
-    print('[FATAL]', datetime.now(), "Can't get data.", sep=',')
+    print("[FATAL]", datetime.now(), "Can't get data.", sep=",")
 except:
-    print('[ERROR]', datetime.now(), pm_2_5,
-          pm_10, aqi_2_5, aqi_10, temp, sep=',')
+    print("[ERROR]", datetime.now(), pm_2_5, pm_10, aqi_2_5, aqi_10, temp, sep=",")
 
 sensor.sleep(sleep=True)
